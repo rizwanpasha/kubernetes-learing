f959b5587fb572984a963002166ee7f16d0e4d7a
Rizwan Pasha <developer.rizwanpasha@gmail.com>
2025-02-07 11:54:20 +0530

    loadbalancer yaml and script update

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-kubectl apply -f gin-server-clusterip-service.yaml
+# kubectl apply -f gin-server-clusterip-service.yaml
 # kubectl apply -f gin-server-pod.yaml
 # kubectl apply -f ping-pod.yaml
+kubectl apply -f gin-server-loadbalancer-service.yaml
 
 # kubectl delete pod gin-server-pod
 # kubectl delete pod ping-pod
 # kubectl delete service gin-server-clusterip-service
+kubectl delete service gin-server-loadbalancer-service
